4a827d2dfc24a41d6d3c42d1b97f03e64eb121fb
Umama <Umamaqasim420@gmail.com>
2022-11-16 00:17:31 +0500

    Updates

---
+++ precache-manifest.a6ae6b93813e044809a5ce92d68d9d0a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7918e912bacf5a6cc5d4",
+    "url": "/umammahh/bonus.git/static/css/main.b2ffe0a6.chunk.css"
+  },
+  {
+    "revision": "7918e912bacf5a6cc5d4",
+    "url": "/umammahh/bonus.git/static/js/main.36b79024.chunk.js"
+  },
+  {
+    "revision": "b0ace2404226e3f18826",
+    "url": "/umammahh/bonus.git/static/js/runtime~main.a1da0467.js"
+  },
+  {
+    "revision": "61e8eef205a37de7cb65",
+    "url": "/umammahh/bonus.git/static/css/2.6fe68b92.chunk.css"
+  },
+  {
+    "revision": "61e8eef205a37de7cb65",
+    "url": "/umammahh/bonus.git/static/js/2.03ded776.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/umammahh/bonus.git/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/umammahh/bonus.git/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/umammahh/bonus.git/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/umammahh/bonus.git/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/umammahh/bonus.git/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "f6617b54d50197dd46b8816618a4b338",
+    "url": "/umammahh/bonus.git/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/umammahh/bonus.git/precache-manifest.a6ae6b93813e044809a5ce92d68d9d0a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/umammahh/bonus.git/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
